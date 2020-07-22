3b4eb51696ba392fc009a9d18f4e6252cbc147a7
iramarJunior <iramar.j@hotmail.com>
2020-07-22 20:26:45 -0300

    Using the default adapter.

--- app/src/main/java/com/iramarfalcao/agenda/MainActivity.kt
+++ app/src/main/java/com/iramarfalcao/agenda/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.iramarfalcao.agenda
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Seja Bem Vindo Iramar Falcão", Toast.LENGTH_LONG).show()
+        contacts_listView.adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, getContacts())
+    }
+
+    private fun getContacts(): List<String> {
+        return listOf(
+            "Junior",
+            "Sueleny",
+            "Rêgo"
+        )
     }
 }
